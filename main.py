46fe4d3cbc1c4f4650b528c55e19035384739abe
Kerem <kerem@oceansync.com>
2023-09-28 10:08:08 -0300

    v4

--- main.py
+++ main.py
@@ -26,40 +26,26 @@ def main(problem):
     for gen in range(1, problem.gen_count):
         print(f"Generation {gen}:")
         
-        # Call Representation methods
+        # Train Generation
+        population.removePopulationGap()
+        
+        children = breeder.breed(population)
+        breeder.compute_individuals_fitness(children)
+
+        # Update the population with children
+        population.replacePopulationGap(children)
+
+        # Call Representation methods here
         representation.display_total_fitness()
         representation.display_highest_fitness()
         representation.display_all_fitness()
         representation.display_highest_representation()
 
-        # Collect fitness scores
+        # Collect fitness scores for plotting
         all_fitness = [individual.fitnessScore for individual in population.individuals]
-        max_fitness = max(all_fitness)
-        
-        max_fitness_percentage = (max_fitness / representation.max_possible_fitness) * 100  # Aligned with Representation class
-
-        print(max_fitness_percentage)
-        # Terminate if any individual's fitness exceeds 85%
-        # if max_fitness_percentage >= 90:  # Adjusted to align with Representation class
-        #     print("Stopping criterion reached.")
-        #     break
-
-        # Train Generation
-        population.removePopulationGap()
-
-        # Calculate the worst and mean score among the remaining individuals
-        worst_among_remaining = min(individual.fitnessScore for individual in population.individuals)
-        mean_among_remaining = sum(individual.fitnessScore for individual in population.individuals) / len(population.individuals)
-
-        # Append to lists for plotting
-        best_scores.append(max_fitness)
-        mean_scores.append(mean_among_remaining)  # Use mean_among_remaining here
-        worst_scores.append(worst_among_remaining)  # Use worst_among_remaining here
-        
-        children = breeder.breed(population)
-        breeder.compute_individuals_fitness(children)
-        population.replacePopulationGap(children)
-
+        best_scores.append(max(all_fitness))
+        mean_scores.append(sum(all_fitness) / len(all_fitness))
+        worst_scores.append(min(all_fitness))
 
     # Plot the fitness scores
     representation.plot_fitness_scores(best_scores, mean_scores, worst_scores)
@@ -76,23 +62,23 @@ if __name__ == "__main__":
     tictactoe_dataset = Dataset("datasets/tic+tac+toe+endgame/tic-tac-toe.data")
 
     # Number of Individuals in the Population
-    population_count = 100
+    population_count = 24
 
     # Max Instruction (Row) per each Individual
-    max_instruction = 24
+    max_instruction = 64
 
     # Operators that will be used
     operators = OperatorSet(['+','-','*2','/2'])
 
     # Max number per each decode instruction (Source Select, Target Index, Source Index)
     # (Max number for the 'operator_select' is dynamically assumed by the OperatorSet class)
-    max_decode_instructions = [2, 4, 4]
+    max_decode_instructions = [2, 4, 9]
 
     # Number of Registers to use
     registerCount = 4
 
     # Percentage of worst fit Individuals to replace
-    gap_percentage = 0.3
+    gap_percentage = 0.2
 
     # Generation Count
     gen_count = 100
@@ -102,7 +88,7 @@ if __name__ == "__main__":
     # 2. Probability of re-initializing an Instruction Bit
     # 3. Probability of randomly appending a new instruction to the child
     # 4. Probability of randomly removing an instruction from the child
-    mutation_prob = [0.1, 0.3, 0.05, 0.05]  # List of 4 probabilities
+    mutation_prob = [0.2, 0.3, 0.1, 0.05]  # List of 4 probabilities
 
 
     ############################
--- modules/Representation.py
+++ modules/Representation.py
@@ -8,18 +8,17 @@ class Representation:
 
 
     def display_total_fitness(self):
-        total_fitness = sum(individual.fitnessScore for individual in self.population.individuals)
-        total_fitness_percentage = (total_fitness / self.max_possible_fitness) * 100
+        total_fitness = sum([individual.fitnessScore for individual in self.population.individuals])
+        total_fitness_percentage = (total_fitness / (self.max_possible_fitness * len(self.population.individuals))) * 100
         print(f"Total Fitness: {total_fitness_percentage:.0f}%")
 
     def display_highest_fitness(self):
-        highest_fitness = max(individual.fitnessScore for individual in self.population.individuals)
+        highest_fitness = max([individual.fitnessScore for individual in self.population.individuals])
         highest_fitness_percentage = (highest_fitness / self.max_possible_fitness) * 100
         print(f"Highest Fitness: {highest_fitness_percentage:.0f}%")
 
     def display_all_fitness(self):
-        all_fitness = [individual.fitnessScore for individual in self.population.individuals]
-        sorted_fitness = sorted(all_fitness, reverse=True)
+        sorted_fitness = sorted([individual.fitnessScore for individual in self.population.individuals], reverse=True)
         sorted_fitness_percentage = [f"{(x / self.max_possible_fitness * 100 ):.0f}%" for x in sorted_fitness]
         print(f"All Fitness Scores (Sorted): {sorted_fitness_percentage}")
 
@@ -29,12 +28,18 @@ class Representation:
         representation = highest_fitness_individual.get_representation()
         print(f"Highest Individual's Representation: {representation}")
 
+
     def plot_fitness_scores(self, best_scores, mean_scores, worst_scores):
+        # Convert raw scores to percentages
+        best_scores_percentage = [(x / self.max_possible_fitness) * 100 for x in best_scores]
+        mean_scores_percentage = [(x / self.max_possible_fitness) * 100 for x in mean_scores]
+        worst_scores_percentage = [(x / self.max_possible_fitness) * 100 for x in worst_scores]
+
         plt.figure(figsize=(10, 6))
-        plt.plot(best_scores, label='Best Fitness', color='#3498DB')  # Blueish
-        plt.plot(mean_scores, label='Mean Fitness', color='#F39C12')  # Yellow/Orange-ish
-        plt.plot(worst_scores, label='Worst Fitness', color='#E74C3C')  # Redish
+        plt.plot(best_scores_percentage, label='Best Fitness', color='#3498DB')  # Blueish
+        plt.plot(mean_scores_percentage, label='Mean Fitness', color='#F39C12')  # Yellow/Orange-ish
+        plt.plot(worst_scores_percentage, label='Worst Fitness', color='#E74C3C')  # Redish
         plt.xlabel('Generation')
-        plt.ylabel('Fitness Score')
+        plt.ylabel('Fitness Score (%)')  # Updated label to indicate percentages
         plt.legend()
-        plt.show()
+        plt.show()  # Keep the plot window open
